4430fe112b2544b99197e25d23b577b0a864535a
noirfierfonce <noirfierfonce@gmail.com>
2026-09-05 21:07:30 +0200

    Brancher l’application au suivi collectif

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.AQ_APP_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbx9ULnqjF3FcPAtsOO9x8CV8QiH5wtjP0aKO9PJqUmJNm-I679iUlijFjg_-RsCjsUGUA/exec",
   appName: "Collage AQ Grenoble"
 };
